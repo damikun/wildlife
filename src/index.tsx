f9daf79b18c0cd9296460dc8fd64356b9fce3985
dalibor <kun.dalibor@gmail.com>
2022-08-16 10:47:38 +0200

    Add Piwik

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import React, { lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import Spinner from './Components/UI/Spinner';
 import reportWebVitals from './reportWebVitals';
+import PiwikPro from '@piwikpro/react-piwik-pro';
+
+PiwikPro.initialize('90133965-b2d2-4f3d-acfb-0fd851e43ee9', 'container-url');
 
 const container = document.getElementById('root');
 
