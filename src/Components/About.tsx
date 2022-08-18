fff51fa5c43bb3ff3cd9abf1143de219606cd659
dalibor <kun.dalibor@gmail.com>
2022-08-18 09:40:38 +0200

    Fixes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,15 +1,15 @@
+import clsx from 'clsx';
 import { useEffect } from 'react';
 import { HomeNavigation } from './Story/Story';
 import { PageViews } from '@piwikpro/react-piwik-pro';
 import { GetUri } from '../Constants';
-import clsx from 'clsx';
 import { Helmet } from 'react-helmet';
 
 export default function About() {
 
   useEffect(() => {
     PageViews.trackPageView('About');
-}, [])
+  }, [])
 
   return (<>
     <Helmet>
@@ -27,13 +27,14 @@ export default function About() {
             content="dalibor kundrat"
         />
     </Helmet>
+
     <Container>
         <HomeNavigation/>
         
         <div className='flex justify-center'>
           <div className='pt-8 px-5 md:pt-10 m-5 text-white max-w-6xl w-full'>
             <div className="space-y-5 text-base lg:text-lg">
-              <p>Foteniu Wildlife sa venujem pretože každa fotka má príbeh. O každej som schopný rozprávať, pretože ma príroda neustále v niečom vyškolí.</p>
+              <p>Foteniu Wildlife sa venujem, pretože každa fotka má príbeh. Príroda ma neustále v niečom vyškolí.</p>
 
               <p>Život je o zažitkoch a príroda vám ich vie nadeliť koľko ste ochotný prijať, no musíte ju rešpektovať. Precestoval som množstvo veľkomest, no kľud a slobodu tam nenájdete.</p>
 
@@ -41,7 +42,7 @@ export default function About() {
 
               <p>Je to v prvom rade záľuba profesne som zo sveta IT.</p>
 
-              <p>Okrem Wildlife rád fotim na horách, pri skalnom lezení a iných vylomeninách.</p>
+              <p>Okrem Wildlife fotim na horách, pri skalnom lezení a iných vylomeninách.</p>
 
               <p className='font-semibold'>Dalibor</p>
 
@@ -52,6 +53,14 @@ export default function About() {
                   <img className="rounded-sm w-1/3" src={GetUri("vybzndca93bpy7c/about_03.webp")} alt="Tatry"/>
 
                   <img className="rounded-sm w-1/3" src={GetUri("6zxkiti3x3q2doe/about_06.webp")} alt="Hory"/>
+                </div>
+
+                <div className='flex w-full space-x-2 overflow-hidden'>
+                  <img className="rounded-sm w-1/3" src={GetUri("n44vr9dnih57f05/About_10.webp")} alt="Sopecne hory"/>
+
+                  <img className="rounded-sm w-1/3" src={GetUri("eol6itffu7ed2eb/About_11.webp")} alt="Rakusko"/>
+
+                  <img className="rounded-sm w-1/3" src={GetUri("50qyk775swms89v/About_12.webp")} alt="Iceland"/>
                 </div>
 
                 <div className='flex flex-col space-y-2 w-full'>
--- src/Components/Story/MorningAfterRain.tsx
+++ src/Components/Story/MorningAfterRain.tsx
@@ -33,14 +33,14 @@ export default function MorningAfterRain(){
 
             <h1 className="text-xl font-semibold">Ráno po daždi</h1>
 
-            <p>Vstávam ráno pred štvrtou a vravím si: <i className="italic font-semibold">"Nechce sa my"</i>. Trochu sa premáham, predsa som sa až tak dobre nevyspal. O polnoci som ešte počúval prichádzajúce búrku a hrmenie s ňou sprevádzané. Práve koly nočnej búrke, ktorá priniesla vodu po dlhotrvajúcom suchu, môže byt ranná scenéria sprevádzanou menšou hmlou a oparom, ktorý je skvelý ako jemne pozadie s peknou vlhkou dynamikou.</p>
+            <p>Vstávam ráno pred štvrtou a vravím si: <i className="italic font-semibold">"Nechce sa my"</i>. Trochu sa premáham, predsa som sa až tak dobre nevyspal. O polnoci som ešte počúval prichádzajúce búrku a hrmenie s ňou sprevádzané. Práve kôly nočnej búrke, ktorá priniesla vodu po dlhotrvajúcom suchu, môže byt ranná scenéria sprevádzanou menšou hmlou a oparom, ktorý je skvelý ako jemne pozadie s peknou vlhkou dynamikou.</p>
             <p>Nechystám sa ďaleko, skôr na istotu, že nejakú vysokú uvidím.</p>
-            <p>Vyrážam na miesto, kde už týždeň po večeroch monitorujem jeleniu zver a jej správanie v období sucha a vysokých denných teplôt.</p>
-            <p>Len včera večer sa mi podarilo nafotiť pekne zábery priamo v kukuricom poli, neďaleko miesta kam práve kráčam.</p>
-            <p>Je šero, svitať začína už o štvrtej, je obdobie najdlhšie dni.</p>
+            <p>Vyrážam na miesto, kde už týždeň po večeroch monitorujem jeleniu zver a jej správanie v období sucha za vysokých denných teplôt.</p>
+            <p>Len včera večer sa mi podarilo nafotiť pekne zábery priamo v kukuričnom poli, neďaleko miesta kam práve kráčam.</p>
+            <p>Je šero, svitať začína už o štvrtej, je obdobie najdlhších dni.</p>
             <p>Fotiť je možné cca. Od 4:30, čaká ma ešte kus cesty. Nezobral som statív, v podstate nič len foťák a dlhé sklo (70-200). Kontrolujem časy a spolieham sa, že to udržím. Odhadujem, že po výstupe kopca to bude 1/80 až 1/100 pri ISO600 no s pribúdajúcim časom sa to zrýchli.</p>
-            <p>Hneď po vyslape kopca spomalím a spozorniem. Počujem aj vlastný buchot srdca. <i className="font-semibold">"Pomaly ďalej zájdeš"</i> a <i className="font-semibold">"Trpezlivosť ruže prináša"</i> je to, čo teraz platí. Nesmiem sa chovať akoby som tu bol len koly fotke. Ta možno bude odmenou, zato, ako sa v lese budem chovať.</p>
-            <p>Za prvým medzi horizontom predo mnou ma byt posed. Nedávno som tam zazrel dvoch sestnastorakov päsť sa v repkovom poli... Hneď za posedom ústí na lúku zvierací chodník a už v minulo ti som tam pozoroval zver prechadzajucu z jedného lesa do druhého v skorých ranných hodinách.</p>
+            <p>Hneď po výšľape kopca spomalím a spozorniem. Počujem aj vlastný buchot srdca. <i className="font-semibold">"Pomaly ďalej zájdeš"</i> a <i className="font-semibold">"Trpezlivosť ruže prináša"</i> je to, čo teraz platí. Nesmiem sa chovať akoby som tu bol len kôly fotke. Ta možno bude odmenou, zato, ako sa v lese budem chovať.</p>
+            <p>Za prvým medzi-horizontom predomnou ma byť posed. Nedávno som tam zazrel dvoch šestnastorákov päsť sa v repkovom poli... Hneď za posedom ústí na lúku zvierací chodník a už v minuloti som tam pozoroval zver prechadzajucu z jedného lesa do druhého v skorých ranných hodinách.</p>
             <div className="w-full justify-center content-center">
                 <iframe
                     className="w-full h-80 md:h-96"
@@ -49,16 +49,16 @@ export default function MorningAfterRain(){
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen />    
             </div>
-            <p>V okolí nič nepočuť len stebotanie ranných vtákov, zvuk lesa a otravne ovady, ktoré ma práve začali neskutočne otravovať, sakra, kde sa tu tak skoro vzali?</p>
-            <p>Vidím ich! Práve vyšli 3 kusy z lesa. Sú, ale ešte ďaleko cca 200m predo mnou. Rozhodujem sa priblížiť, čo to pôjde.</p>
-            <p>Jelene majú poranu jedinú starosť, poriadne sa nažrať, a tak predpokladam, že si ma nevšimnú.</p>
-            <p>Ráno sú naozaj menej obozretne, nebývajú v tento čas bežné rusene, ako podvečer kedy sledujú všetko okolo a vyhodnocujú každý zvuk. Už nejakú tu hodinku pobehujú a mali tak dostatok času adaptovať sa na zvuky prostredia.</p>
+            <p>V okolí nič nepočuť len štebotanie ranných vtákov, zvuk lesa a otravne ovady, ktoré ma práve začali neskutočne otravovať, sakra, kde sa tu tak skoro vzali?</p>
+            <p>Vidím ich! Práve vyšli 3 kusy z lesa. Sú, ale ešte ďaleko cca 200m predomnou. Rozhodujem sa priblížiť, čo to pôjde.</p>
+            <p>Jelene majú poránu jedinú starosť, poriadne sa nažrať, a tak predpokladam, že si ma nevšimnú.</p>
+            <p>Ráno sú naozaj menej obozretne, nebývajú v tento čas bežné rušene, ako tomu býva podvečer, kedy sledujú všetko okolo a vyhodnocujú každý zvuk a pohyb. Už nejakú hodinku pobehujú a mali tak dostatok času adaptovať sa na zvuky prostredia.</p>
             <p>Pokrčený sa pomaly približujem z ľavá ma kryje repka z práva les. Kráčam bez obáv, nakoľko pršalo, pod nohami nič nešuští a nepraská. Navyše v lese ešte počuť dopadať kvapky vody, čo zakrýva drobný šum.</p>
-            <p>Zrazu pri zemi, asi 5m predo mnou vykukne z poľa zvieracia hlava, vravím si <i>"asi jazvec"</i>... <i>"no prd"</i>... Vybehne srnec a pozeráme na seba...</p>
-            <p>Samozrejme tvárim sa ako strom (haha)... Nezožral to, aj keď mám maskovanie... Skočil do lesa a začala pobrechavat... Nepáčilo sa mu to.  Momentálne je ich poľovná sezóna a akoby sa po nich zľahla zem. Vraví sa, že to vedia a <i>"Ahaho"</i> musí sa objaviť práve teraz. Nuž priestor  je to jeho a ja som tu ten nezelany hosť. Už nič nenarobím, nečakal som ho a evidentne ani on mňa.</p>
+            <p>Zrazu pri zemi, asi 5m predomnou vykukne z poľa zvieracia hlava. Vravím si, <i>"asi jazvec"</i>... <i>"no prd"</i>... Vybehne srnec a pozeráme na seba...</p>
+            <p>Samozrejme tvárim sa ako strom (haha)... Nezožral to, aj keď mám maskovanie... Skočil do lesa a začala pobrechavat... Nepáčilo sa mu to.  Momentálne je ich poľovná sezóna a akoby sa po nich zľahla zem. Vraví sa, že to vedia a <i>"Ahaho"</i> musí sa objaviť práve teraz. Nuž priestor je to jeho a ja som tu ten neželaný hosť. Už nič nenarobím, nečakal som ho a evidentne ani on mňa.</p>
             <p>Zdvíham pomaly hlavu nad repku a snažím sa zistiť, čo nato jelene, pri ktorých som menej než 100m.</p>
             <p>Na brechot našťastie nezareagovali poobzerali sa a ďalej pokračovali v pastve.</p>
-            <p>To, že nie vždy jelene reagujú na výstražné signály srnčie zvery (ako je brechot) už pozorujem opakovane a nijak konkrétne ma to neprekvapí. Opačne to, ale neplatí! Srnčia zver reaguje veľmi pozorne na jelenie výstražné signály. Práve, preto sa pri nich cíti bezpečne. Dalo sa, teda aj predpokladať, že nejaká ta srnčia v ich okolí bude.</p>
+            <p>To, že nie vždy jelene reagujú na výstražné signály srnčej zvery (ako je brechot) už pozorujem opakovane a nijak konkrétne ma to neprekvapí. Opačne to, ale neplatí! Srnčia zver reaguje veľmi pozorne na jelenie výstražné signály. Práve, preto sa pri nich cíti bezpečne. Dalo sa, teda aj predpokladať, že nejaká ta srnčia v ich okolí bude.</p>
             <p>Podarilo sa mi priblížiť na cca. 50m. To je asi tentokrát maximum, pretože jeden z mladších jeleňov si ma už všimol no nevie ma identifikovať, preto zvedavo pózuje a ja zatiaľ fotím par pekných záberov s poľovníckym posedom v jeho pozadí.</p>
             
             <img className="rounded-sm shadow-sm" src={deer} alt="Pred posedom"/>
@@ -68,7 +68,7 @@ export default function MorningAfterRain(){
             
             <img className="rounded-sm" src={deers} alt="Crieda jelenov"/>
 
-            <p>Počas priblizovania som ich vôbec nevidel pravdepodobne polihavali a až brechot srny alebo pohyb starších ich mohol postaviť.  Nieje to prvýkrát, čo bola vysoká vo večsom počte odomna na par metrov a vôbec ju nebolo počuť je to fascinujúce s akou tichosťou sa dokážu v takom poraste pohybovať. Repka je pomerne hustá a dáva im istú mieru bezpečia, vlk by sa tam hádam načisto zamotal a možno by do nej ani nevošiel. Sám som si to raz chcel cez podobne pole <i>"Skrátiť"</i> a par metrov ma stalo riadne množstvo energie.</p>
+            <p>Počas približovania som ich vôbec nevidel pravdepodobne polihavali a až brechot srnčej alebo pohyb starších ich mohol postaviť.  Nieje to prvýkrát, čo bola vysoká vo večšom počte na par metrov a vôbec ju nebolo počuť. Je to fascinujúce s akou tichosťou sa dokážu v takom poraste pohybovať. Repka je pomerne hustá a dáva im istú mieru bezpečia, vlk by sa tam hádam načisto zamotal a možno by do nej ani nevošiel. Sám som si to raz chcel cez podobne pole <i>"Skrátiť"</i> a par metrov ma stalo riadne množstvo energie.</p>
             <p>Po krátkom čase jelene pomaly zachádzajú do lesa a ja si vravím, že ešte zbehnem pozrieť kukuričné pole z predošlého večera.</p>
             <p>Bol to pokus... Nič konkrétne nevidím a po 10minútach sledovania zahajujem návrat.</p>
         
