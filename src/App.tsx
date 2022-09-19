b308edb337f17339b24995051d79fca2ebe30f8f
dalibor <kun.dalibor@gmail.com>
2022-09-19 08:35:41 +0200

    Add Missing link mapping to Q3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from './Components/About';
 import Providers from './Utils/Providers';
 import Quartal from './Components/Quartal';
 import Story from './Components/Story/Story';
-import { ImagesQ1, ImagesQ2 } from './Constants';
+import { ImagesQ1, ImagesQ2, ImagesQ3 } from './Constants';
 import {Navigate, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/2022/Q1" element={<Quartal images={ImagesQ1} name='Q1 2022'/>} />
         <Route path="/2022/Q2" element={<Quartal images={ImagesQ2} name='Q2 2022'/>} />
+        <Route path="/2022/Q3" element={<Quartal images={ImagesQ3} name='Q3 2022'/>} />
         <Route path="/Story/*" element={<Story />} />
         <Route path="/AboutMe" element={<About />} />
         <Route path="/" element={<Home />} />
